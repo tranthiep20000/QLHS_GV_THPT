eaef546efd2822375663e26bcb969af4ece4a898
longkieuck <longkieuck@gmail.com>
2021-02-25 18:54:11 +0700

    add MonHocDAO class and frmMonHoc

--- QLHS_GV_THPT/QLHS_GV_THPT/GUI/frmMonHoc.cs
+++ QLHS_GV_THPT/QLHS_GV_THPT/GUI/frmMonHoc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLHS_GV_THPT.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,53 @@ namespace QLHS_GV_THPT.GUI
 {
     public partial class frmMonHoc : Form
     {
+        BindingSource monHocList = new BindingSource();
         public frmMonHoc()
         {
             InitializeComponent();
+            LoadData();
+        }
+        
+        private void LoadData()
+        {
+            dgvMonHoc.DataSource = monHocList;
+            LoadListMonHoc();
+            EditDataGridViewHeader();
+        }
+
+        private void EditDataGridViewHeader()
+        {
+            dgvMonHoc.Columns["IdMonHoc"].HeaderText = "ID Môn học";
+            dgvMonHoc.Columns["TenMonHoc"].HeaderText = "Tên môn học";
+            dgvMonHoc.Columns["SoTietHoc"].HeaderText = "Số tiết học";
+            dgvMonHoc.Columns["NamHoc"].HeaderText = "Năm học";
+            dgvMonHoc.Columns["KiHoc"].HeaderText = "Kì học";
+            dgvMonHoc.Columns["IdGiaoVien"].HeaderText = "Id giáo viên";
+        }
+
+        private void LoadListMonHoc()
+        {
+            monHocList.DataSource = MonHocDAO.Instance.GetAll();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
