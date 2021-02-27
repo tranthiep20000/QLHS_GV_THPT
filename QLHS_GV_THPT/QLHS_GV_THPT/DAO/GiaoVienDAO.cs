e9c126bb2b37597659cfe36a58afe90245ce02cd
dat78787878 <d782000@gmail.com>
2021-02-27 23:08:17 +0700

    form LopHoc: add BindingData

---
+++ QLHS_GV_THPT/QLHS_GV_THPT/DAO/GiaoVienDAO.cs
@@ -0,0 +1,33 @@
+﻿using QLHS_GV_THPT.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHS_GV_THPT.DAO
+{
+    class GiaoVienDAO
+    {
+        private static GiaoVienDAO instance;
+
+        internal static GiaoVienDAO Instance
+        {
+            get { if (instance == null) instance = new GiaoVienDAO(); return instance; }
+            private set { instance = value; }
+        }
+        public List<GiaoVienDTO> GetAll()
+        {
+            List<GiaoVienDTO> list = new List<GiaoVienDTO>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("SP_GiaoVien_GetAll");
+            foreach (DataRow item in data.Rows)
+            {
+                GiaoVienDTO entry = new GiaoVienDTO(item);
+                list.Add(entry);
+            }
+            return list;
+        }
+
+    }
+}
--- QLHS_GV_THPT/QLHS_GV_THPT/GUI/frmLopHoc.cs
+++ QLHS_GV_THPT/QLHS_GV_THPT/GUI/frmLopHoc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QLHS_GV_THPT.DAO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,77 @@ namespace QLHS_GV_THPT.GUI
 {
     public partial class frmLopHoc : Form
     {
+        BindingSource lopHocList = new BindingSource();
+
         public frmLopHoc()
         {
             InitializeComponent();
+            LoadFirstTime();
+
         }
+        private void LoadFirstTime()
+        {
+            dgvLopHoc.DataSource = lopHocList;
+            LoadListLopHoc();
+            EditDataGridView();
+            LoadComboboxIdGiaoVienChuNhiem();
+            BindingDataToFrom();
+        }
+        private void LoadListLopHoc()
+        {
+            lopHocList.DataSource = LopHocDAO.Instance.GetAll();
+        }
+        private void EditDataGridView()
+        {
+            dgvLopHoc.Columns["IdLopHoc"].HeaderText = "ID Lớp học";
+            dgvLopHoc.Columns["TenLopHoc"].HeaderText = "Tên lớp học";
+            dgvLopHoc.Columns["TenKhoi"].HeaderText = "Tên khối";
+            dgvLopHoc.Columns["NamHoc"].HeaderText = "Năm học";
+            dgvLopHoc.Columns["IdGiaoVienChuNhiem"].HeaderText = "ID Giáo viên chủ nhiệm";
+        }
+        private void BindingDataToFrom()
+        {
+            txtIdLopHoc.DataBindings.Add(new Binding("Text", dgvLopHoc.DataSource, "IdLopHoc", true, DataSourceUpdateMode.Never));
+            txtTenLopHoc.DataBindings.Add(new Binding("Text", dgvLopHoc.DataSource, "TenLopHoc", true, DataSourceUpdateMode.Never));
+            txtTenKhoi.DataBindings.Add(new Binding("Text", dgvLopHoc.DataSource, "TenKhoi", true, DataSourceUpdateMode.Never));
+            txtNamHoc.DataBindings.Add(new Binding("Text", dgvLopHoc.DataSource, "NamHoc", true, DataSourceUpdateMode.Never));
+            cboIdGiaoVien.DataBindings.Add(new Binding("Text", dgvLopHoc.DataSource, "IdGiaoVienChuNhiem", true, DataSourceUpdateMode.Never));
+        }
+        private void LoadComboboxIdGiaoVienChuNhiem()
+        {
+            cboIdGiaoVien.DataSource = GiaoVienDAO.Instance.GetAll();
+            cboIdGiaoVien.DisplayMember = "IdGiaoVien";
+        }
+        private void MakeNull()
+        {
+            txtIdLopHoc.Text = "";
+            txtTenLopHoc.Text = "";
+            txtTenKhoi.Text = "";
+            txtNamHoc.Text = "";
+            txtTimKiem.Text = "";
+            LoadComboboxIdGiaoVienChuNhiem();
+        }
+
+        private void btnLamTrong_Click(object sender, EventArgs e)
+        {
+            MakeNull();
+        }
+
+        private void btnTimKiem_Click(object sender, EventArgs e)
+        {
+            string str = txtTimKiem.Text.Trim();
+            if (str == "")
+            {
+                MessageBox.Show("Chưa nhập thông tin tìm kiếm");
+                return;
+            }
+            lopHocList.DataSource = LopHocDAO.Instance.Search(str);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            LoadListLopHoc();
+        }
+
     }
 }
