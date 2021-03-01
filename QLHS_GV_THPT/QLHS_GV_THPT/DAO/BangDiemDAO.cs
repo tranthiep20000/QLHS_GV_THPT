37cdf4f7a8783f0e8a2408a1e5a014df84bdc471
hien.van.nguyen0408@gmail.com <xamlone321@gmail.com>
2021-03-01 14:30:45 +0700

    thêm hàm update , delete , search dữ liệu từ Database phục vụ tính ở form điểm

--- QLHS_GV_THPT/QLHS_GV_THPT/DAO/BangDiemDAO.cs
+++ QLHS_GV_THPT/QLHS_GV_THPT/DAO/BangDiemDAO.cs
@@ -67,6 +67,28 @@ namespace QuanLyHocSinhTHPT.DAO
             int result = DataProvider.Instance.ExecuteNonQuery("SP_BangDiem_Insert @diem15phut , @diem45phut , @diemHocKi , @idMonHoc , @idHocSinh", new object[] { diem15phut, diem45phut, diemHocKi, idMonHoc, idHocSinh });
             return result > 0;
         }
-        
+        public bool Update(int idBangDiem, float diem15phut, float diem45phut, float diemHocKi, int idMonHoc, int idHocSinh)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_BangDiem_Update @idBangDiem , @diem15phut , @diem45phut , @diemHocKi , @idMonHoc , @idHocSinh", new object[] { idBangDiem, diem15phut, diem45phut, diemHocKi, idMonHoc, idHocSinh });
+            return result > 0;
+        }
+        public bool Delete(int idBangDiem)
+        {
+            int result = DataProvider.Instance.ExecuteNonQuery("SP_BangDiem_Delete @idBangDiem", new object[] { idBangDiem });
+
+            return result > 0;
+        }
+        public List<BangDiemDTO> Search(string searchValue)
+        {
+            List<BangDiemDTO> list = new List<BangDiemDTO>();
+            DataTable data = DataProvider.Instance.ExecuteQuery("SP_BangDiem_Search @searchValue", new object[] { searchValue });
+            foreach (DataRow item in data.Rows)
+            {
+                BangDiemDTO entry = new BangDiemDTO(item);
+                list.Add(entry);
+            }
+            return list;
+        }
+
     }
 }
